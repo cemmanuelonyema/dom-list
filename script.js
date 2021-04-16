876703b837a0e4361a07d9ddfd4ee1ac1a24f88d
C. Emmanuel Onyema <cemmanuelonyema@gmail.com>
2021-04-16 04:15:30 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,64 @@
+//variables
+const userInput = document.getElementById('userinput');
+const btnEnter = document.getElementById('enter');
+
+//functions
+function userInputValue() {
+  return userInput.value;    
+}
+
+function createListElement() {
+    const li = document.createElement('li');
+    const ul = document.querySelector('ul');
+
+    li.appendChild(document.createTextNode(userInput.value));
+    ul.appendChild(li);
+    userInput.value = '';    
+}
+
+function addEventAfterClick() {
+    if (userInputValue() !== '' ) {
+        createListElement();
+    }  
+}
+
+function addEventAfterKeypress(event) {
+    if ( userInputValue() && event.key === 13 ) {
+        createListElement();
+    }
+}
+
+//////////////////
+
+btnEnter.addEventListener('click', addEventAfterClick)
+userInput.addEventListener('keypress', addEventAfterKeypress)
+
+
+
+
+
+/* The loger method without using functions
+
+btnEnter.addEventListener('click', function(){
+    if (userInput.value !== '' ) {
+        const li = document.createElement('li');
+        const ul = document.querySelector('ul');
+
+        li.appendChild(document.createTextNode(userInput.value));
+        ul.appendChild(li);
+        userInput.value = '';
+    }
+})
+
+userInput.addEventListener('keypress', function(event){
+    if (event.key === 13 ) {
+        const li = document.createElement('li');
+        const ul = document.querySelector('ul');
+
+        li.appendChild(document.createTextNode(userInput.value));
+        ul.appendChild(li);
+        userInput.value = '';
+    }
+})
+
+*/
